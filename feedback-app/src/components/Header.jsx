cd16f154b9914d32c7cef94ea60c3e7f88b87248
Pandey17Shashwat <shashwatpandey161@gmail.com>
2023-01-23 05:16:06 +0530

    some work has been done

---
+++ feedback-app/src/components/Header.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+
+function Header({text,bgColor,textColor}) {
+    const headerStyles = {
+        backgroundColor : 'blue',
+        color : 'red',
+    }
+
+  return (
+    <header style={headerStyles}>
+        <div className='container'>
+            <h2>{text}</h2>
+        </div>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+    text: 'feedback - UI',
+    textColor : '#ff6a95',
+    bgColor : 'rgba(0,0,0,0,4)',
+}
+
+Header.propTypes = {
+    bgColor : PropTypes.string,
+    textColor : PropTypes.string,
+    text : PropTypes.string,
+}
+
+export default Header
