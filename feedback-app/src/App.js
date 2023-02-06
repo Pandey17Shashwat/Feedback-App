5b6f4eb0323ee74f6e7bca136b016f1e00e5c1b6
Pandey17Shashwat <shashwatpandey161@gmail.com>
2023-02-06 15:17:34 +0530

    FeedbackForm

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -3,6 +3,7 @@ import { Component, useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats'; 
+import FeedbackForm from './components/FeedbackForm';
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -16,6 +17,7 @@ function App() {
         <>
             <Header />
             <diV className="container">
+                <FeedbackForm />
                 <FeedbackStats feedback = {feedback} />
                 <FeedbackList feedback={feedback} handleDelete = {deleteFeedback}/>
             </diV>
